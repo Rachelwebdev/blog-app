17ee6993d072edb0e4b43100a752403b8641c4a3
Rachel Isaac <rachelisaac995@gmail.com>
2023-05-04 19:00:03 +0400

    Add quotes around post and user methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,6 @@ class Post < ApplicationRecord
   end
 
   def last_five_comments
-    comments.order(updated_at: :desc).limit(5)
+    comments.order('updated_at: :desc').limit(5)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'author_id'
 
   def last_three_posts
-    posts.order(updated_at: :desc).limit(3)
+    posts.order('updated_at: :desc').limit(3)
   end
 end
