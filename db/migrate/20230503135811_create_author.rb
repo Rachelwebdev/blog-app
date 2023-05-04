744a0ba333288f0444afa16b0244e495078ea988
Rachel Isaac <rachelisaac995@gmail.com>
2023-05-04 12:44:30 +0400

    update schema file

--- db/migrate/20230503135811_create_author.rb
+++ db/migrate/20230503135811_create_author.rb
@@ -1,8 +1,7 @@
 class CreateAuthor < ActiveRecord::Migration[7.0]
   def change
-    create_table :authors do |t|
+    create_table :users do |t|
       t.string :name
-      t.string :email
       t.string :photo
       t.string :bio
       t.integer :post_count
--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_03_135928) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "authors", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "email"
     t.string "photo"
     t.string "bio"
     t.integer "post_count"
@@ -26,8 +25,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_03_135928) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
-    t.bigint "post_id"
-    t.bigint "author_id"
+    t.bigint "post_id", null: false
+    t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_comments_on_author_id"
@@ -35,8 +34,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_03_135928) do
   end
 
   create_table "likes", force: :cascade do |t|
-    t.bigint "post_id"
-    t.bigint "author_id"
+    t.bigint "post_id", null: false
+    t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_likes_on_author_id"
@@ -46,7 +45,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_03_135928) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.bigint "author_id"
+    t.bigint "author_id", null: false
     t.integer "comments_counter"
     t.integer "likes_counter"
     t.datetime "created_at", null: false
