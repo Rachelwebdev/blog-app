80cf24ee0caf2ba2cdcea017730c1dcd870c2b28
Rachel Isaac <rachelisaac995@gmail.com>
2023-05-05 12:13:36 +0400

    Validate post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,16 @@ class Post < ApplicationRecord
   has_many :comments
   after_save :update_post_count
 
+  validates :title, presence:true, length: {maximum: 250}
+  validates :comments_counter, presence:true, numericality: {
+    only_integer: true, 
+    greater_than_or_equal_to: 0
+  } 
+  validates :likes_counter, presence:true, numericality: {
+    only_integer: true, 
+    greater_than_or_equal_to: 0
+  }
+
   def update_post_count
     author.increment!(:post_count)
   end
