ff825b752551a89d2ee73370bee38c3838780fbe
Rachel Isaac <rachelisaac995@gmail.com>
2023-05-11 19:44:36 +0400

    Add form to blog app

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,25 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @user = User.find(params[:user_id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      flash[:success] = 'Post created successfully'
+      redirect_to "/users/#{current_user.id}/posts"
+    else
+      flash[:danger] = "Couldn't create post"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
