c7233a15bc7fc7cf6f6c632371bc44bac7617d17
Rachel Isaac <rachelisaac995@gmail.com>
2023-05-17 09:22:19 +0400

    Fix n+1 problems

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def index
-    @user = User.includes(:posts).find(params[:user_id])
+    @posts = Post.all
+    @user = User.includes(:posts, :comments).find(params[:user_id])
   end
 
   def show
