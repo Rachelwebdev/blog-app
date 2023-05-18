d2517098e11fecc12ac9650c6682693b6509fb8a
Rachel Isaac <rachelisaac995@gmail.com>
2023-05-18 12:09:55 +0400

    Fix linter errors

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '3.2.2'
 
+gem 'rubocop'
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem 'rails', '~> 7.0.4', '>= 7.0.4.3'
 
@@ -53,7 +55,6 @@ group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
   gem 'rails-controller-testing'
-  gem 'rspec-rails'
 end
 
 group :development do
--- spec/system/users/index_spec.rb
+++ spec/system/users/index_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 
 RSpec.describe 'users/index', type: :feature do
   before(:each) do
-    
     @user1 =
       User.create(
         name: 'Tom',
@@ -13,28 +12,28 @@ RSpec.describe 'users/index', type: :feature do
       )
 
 
-       @user2 = User.create(
-        name: 'Jerry',
-        photo: 'https://images.unsplash.com/photo-1510915228340-29c85a43dcfe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-        bio: 'Software Developer from South Africa', post_count: 3
-      )
+    @user2 = User.create(
+      name: 'Jerry',
+      photo: 'https://images.unsplash.com/photo-1510915228340-29c85a43dcfe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+      bio: 'Software Developer from South Africa', post_count: 3
+    )
 
-      visit '/users' 
-    end
-    
+    visit '/users'
+  end
+
 
   it 'shows the name of all users' do
-      expect(page).to have_content('Jerry')
+    expect(page).to have_content('Jerry')
   end
 
   it 'display the profile picture for each user' do
-      expect(page).to have_selector("img[src='#{@user2.photo}']")    
+    expect(page).to have_selector("img[src='#{@user2.photo}']")
   end
-  Please kindly add the test to this file spec/system/users/index_spec.rb
+  Please kindly add the test to this file spec / system / users / index_spec.rb
 
   it 'displays number of posts each user has written' do
-      expect(page).to have_content "Number of posts: #{@user2.post_count}"
-  end 
+    expect(page).to have_content "Number of posts: #{@user2.post_count}"
+  end
 
   it 'redirects to to a users show page when a user is clicked' do
     click_on @user2.name
