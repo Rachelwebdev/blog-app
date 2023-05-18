e2e8588c380440ea9720ad3276b385f3542da96d
Rachel Isaac <rachelisaac995@gmail.com>
2023-05-18 16:41:41 +0400

    Update integration test

--- spec/system/users/index_spec.rb
+++ spec/system/users/index_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'users/index', type: :feature do
   before(:each) do
-    @user1 =
+    @user1 = 
       User.create(
         name: 'Tom',
         photo: 'https://images.unsplash.com/photo-1510915228340-29c85a43dcfe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
@@ -15,24 +15,29 @@ RSpec.describe 'users/index', type: :feature do
       photo: 'https://images.unsplash.com/photo-1510915228340-29c85a43dcfe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
       bio: 'Software Developer from South Africa', post_count: 3
     )
-
+  @users = User.all
     visit '/users'
   end
 
   it 'shows the name of all users' do
-    expect(page).to have_content('Jerry')
+    @users.each do |user|
+      expect(page).to have_content(user.name)
+    end
   end
 
   it 'display the profile picture for each user' do
-    expect(page).to have_selector("img[src='#{@user2.photo}']")
+    @users.each do |user|
+      expect(page).to have_selector("img[src='#{user.photo}']")
+    end
   end
-  Please kindly add the test to this file spec / system / users / index_spec.rb
 
   it 'displays number of posts each user has written' do
-    expect(page).to have_content "Number of posts: #{@user2.post_count}"
+    @users.each do |user|
+     expect(page).to have_content "Number of posts: #{user.post_count}"
+    end
   end
 
-  it 'redirects to to a users show page when a user is clicked' do
+  it 'redirects to to a users show page when a user is clicked' do    
     click_on @user2.name
     expect(page).to have_current_path user_path(@user2.id)
   end
