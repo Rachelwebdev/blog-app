9b92d693aa44074c5f4385602c8682af6b4c2bd3
Rachel Isaac <rachelisaac995@gmail.com>
2023-05-18 16:42:15 +0400

    Fix linter errors

--- spec/system/users/index_spec.rb
+++ spec/system/users/index_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'users/index', type: :feature do
   before(:each) do
-    @user1 = 
+    @user1 =
       User.create(
         name: 'Tom',
         photo: 'https://images.unsplash.com/photo-1510915228340-29c85a43dcfe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
@@ -15,7 +15,7 @@ RSpec.describe 'users/index', type: :feature do
       photo: 'https://images.unsplash.com/photo-1510915228340-29c85a43dcfe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
       bio: 'Software Developer from South Africa', post_count: 3
     )
-  @users = User.all
+    @users = User.all
     visit '/users'
   end
 
@@ -33,11 +33,11 @@ RSpec.describe 'users/index', type: :feature do
 
   it 'displays number of posts each user has written' do
     @users.each do |user|
-     expect(page).to have_content "Number of posts: #{user.post_count}"
+      expect(page).to have_content "Number of posts: #{user.post_count}"
     end
   end
 
-  it 'redirects to to a users show page when a user is clicked' do    
+  it 'redirects to to a users show page when a user is clicked' do
     click_on @user2.name
     expect(page).to have_current_path user_path(@user2.id)
   end
